bbc052f0df8345c3ab9353a16f0bc11b82e30fd1
LauraKett <laurakettle94@hotmail.com>
2026-05-11 19:50:18 +0100

    updated navbar icons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react"; 
+import searchIcon from "../assets/searchIcon.svg";
 
 
 function Navbar() {
@@ -23,10 +24,18 @@ function Navbar() {
             <div className="nav-links">
                 <Link to="/">Home</Link>
                 <Link to="/rooms">Rooms</Link>
-                <Link to="/search">Search</Link>
-                <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
                 <Link to="/dashboard">Dashboard</Link>
+            </div>
+
+            <div className="nav-actions">
+                <Link to="/search" className="search-link">
+                <img src={searchIcon} alt="search" className="search-icon" />
+                </Link>
+
+                <Link to="/login" className="login-button">Login
+                </Link>
+
             </div>
 
         </nav>
