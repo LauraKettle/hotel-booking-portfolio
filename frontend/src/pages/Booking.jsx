01f440af18ae560cf965f6c43b624fa71c93baca
LauraKett <laurakettle94@hotmail.com>
2026-05-19 18:34:48 +0100

    search page updated

--- frontend/src/pages/Booking.jsx
+++ frontend/src/pages/Booking.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import "index.css";
+// import "index.css";
 
 function RoomBooking() {
   const { id } = useParams();
