eaf7fa736684e650a9c7c8c5919ecabdf7886c2b
LauraKett <laurakettle94@hotmail.com>
2026-05-20 17:56:57 +0100

    made site responsive with media queries

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react"; 
 import searchIcon from "../assets/searchIcon.svg";
+import menuIcon from "../assets/menuIcon.svg";
+import closeIcon from "../assets/closeIcon.svg";
 
 
 function Navbar() {
 
     const [scrolled, setScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const location = useLocation();  
     const isHomePage = location.pathname === "/";
     
@@ -21,14 +25,20 @@ function Navbar() {
         };
     }, []);
 
+        function closeMenu() {
+            setMenuOpen(false);
+        }
+
     return(
         <nav className={`navbar ${scrolled ? "scrolled" : ""} ${!isHomePage ? "other-page" : ""}`}>
-            <div className="nav-links">
-                <Link to="/">Home</Link>
-                <Link to="/rooms">Rooms</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/dashboard">Dashboard</Link>
+
+            <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+                <Link to="/" onClick={closeMenu}>Home</Link>
+                <Link to="/rooms" onClick={closeMenu}>Rooms</Link>
+                <Link to="/register" onClick={closeMenu}>Register</Link>
+                <Link to="/dashboard" onClick={closeMenu}>Dashboard</Link>
             </div>
+
 
             <div className="nav-actions">
                 <Link to="/search" className="search-link">
@@ -37,6 +47,13 @@ function Navbar() {
 
                 <Link to="/login" className="login-button">Login
                 </Link>
+
+                <button className="burger-button" 
+                        onClick={() => setMenuOpen((previousState) => !previousState)}>
+                <img src={menuOpen ? closeIcon : menuIcon} 
+                     alt={menuOpen ? "Close menu" : "Open Menu"}
+                     className="burger-menu" />
+                </button>
 
             </div>
 
